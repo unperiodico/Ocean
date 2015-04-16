a1742a83467d0f660b5e5566d82ae3850b252d40
Nick <Nick@xiaoxuedongmanketangdeiMac.local>
2015-04-16 09:08:03 +0800

    新增帖子列表，帖子详情页面

---
+++ PostListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostListViewController.h
+//  Ocean
+//
+//  Created by Nick on 15/4/16.
+//  Copyright (c) 2015年 xyzx5u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostListViewController : UITableViewController
+
+@end
